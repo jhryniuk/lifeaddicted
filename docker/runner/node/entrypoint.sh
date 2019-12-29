902510a6c320adb139ad71eaf729805fd7218c9f
Jan Hryniuk <jasiekhryniuk@gmail.com>
2019-12-29 19:39:46 +0100

    Security fix.

--- docker/runner/node/entrypoint.sh
+++ docker/runner/node/entrypoint.sh
@@ -2,15 +2,18 @@
 
 cd /var/www
 
-#npm install -g @angular/cli
+npm install -g @angular/cli
 #
 #ng new lifeaddicted --skipTests=true --directory=./ --style=scss --routing
 
+npm update
+npm audit fix --force
 npm install
 chmod 777 ./
 npm audit fix
 
-npm run-script start
+#npm run-script start
+ng serve --host 0.0.0.0
 
 #npm run-script build
 #chmod 777 -R dist
